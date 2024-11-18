718cbc40d8b3136b54d7c0605cbe87f630190010
anatoly_bobrov <acrocanthbm@gmail.com>
2024-11-18 16:30:01 +0300

    Добавлен класс TestClassforGit

---
+++ tests(backend)/src/enums/user_enums.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class Genders(Enum):
+    male = "male"
+    female = "female"
+
+
+class Statuses(Enum):
+    active = "active"
+    inactive = "inactive"
+
+
+class UserErrors(Enum):
+    WrongEmail = "Wrong e-mail"
+
+
+class Test_class_for_GIT(Enum):
+    pass
